af7641b205132271f158f3813684c01b67724e1a
Alexander Arkhangelskiy <aarkhan@ya.ru>
2019-10-06 16:38:13 +0300

    Add static libraries to Travis-CI

--- travis-build.sh
+++ travis-build.sh
@@ -6,11 +6,16 @@ rm -rf build.paho
 mkdir build.paho
 cd build.paho
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
-cmake -DCMAKE_BUILD_TYPE=Debug -DPAHO_WITH_SSL=TRUE -DPAHO_BUILD_DOCUMENTATION=FALSE -DPAHO_BUILD_SAMPLES=TRUE ..
-make
+cmake --version
+cmake -S .. \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+	-DPAHO_BUILD_STATIC=$BUILD_STATIC \
+	-DPAHO_WITH_SSL=TRUE \
+	-DPAHO_BUILD_DOCUMENTATION=FALSE \
+	-DPAHO_BUILD_SAMPLES=TRUE
+cmake --build . --config Debug
 python3 ../test/mqttsas.py &
 ctest -VV --timeout 600
 cpack --verbose
 kill %1
 #killall mosquitto
-
